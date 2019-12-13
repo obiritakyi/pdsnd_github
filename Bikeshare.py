31aabb1ac78506d2ad79a347a962437c940a1ab8
Your-Full-Name <jayxpoza@gmail.com>
2019-12-13 16:44:38 +0000

    Introduce variable under get_filters function

--- Bikeshare.py
+++ Bikeshare.py
@@ -43,7 +43,7 @@ def get_filters():
 
         # get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
-        day = input('Lastly, type any day of the week you want to explore. \n(e.g. all, tuesday, friday) \n> ')
+        day = input('Lastly, type any day of the week you want to explore. \n(e.g. all, monday, tuesday, friday) \n> ')
         if day in DAYS:
             break
 
